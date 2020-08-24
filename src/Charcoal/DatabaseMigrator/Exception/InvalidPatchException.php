a16d177583a758dcf76bbc88cf55cbf2109e9746
Joel Alphonso <joel@locomotive.ca>
2020-08-24 16:35:28 -0400

    feat(exception): add InvalidPatchException

---
+++ src/Charcoal/DatabaseMigrator/Exception/InvalidPatchException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Charcoal\DatabaseMigrator\Exception;
+
+/**
+ * Invalid Patch Exception
+ *
+ * Thrown when a Patch cannot be instantiated.
+ */
+class InvalidPatchException extends \Exception
+{
+}
